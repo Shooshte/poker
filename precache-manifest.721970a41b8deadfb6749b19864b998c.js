1dbdc172c37fcf4147428910b5be9f73e78a5f82
shooshte <miha.sustersic.work@gmail.com>
2019-09-10 13:41:12 +0200

    Updates

---
+++ precache-manifest.721970a41b8deadfb6749b19864b998c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26ed89074b79a9f0d09d0c53b0a12a2",
+    "url": "/poker/index.html"
+  },
+  {
+    "revision": "6c04e1646ad541693795",
+    "url": "/poker/static/css/main.b10e6f9b.chunk.css"
+  },
+  {
+    "revision": "173b59ff3d05e3a3a649",
+    "url": "/poker/static/js/2.23e55110.chunk.js"
+  },
+  {
+    "revision": "6c04e1646ad541693795",
+    "url": "/poker/static/js/main.5885a345.chunk.js"
+  },
+  {
+    "revision": "14cd5ac7dc653ccaadc0",
+    "url": "/poker/static/js/runtime~main.5a8456e2.js"
+  }
+]);
